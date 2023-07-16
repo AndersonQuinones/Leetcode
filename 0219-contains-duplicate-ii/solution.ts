00b8e1b2985555b4314be7a8a982e8fd2f8b6d74
Anderson Quinones <32976024+AndersonQuinones@users.noreply.github.com>
2023-07-16 12:21:58 -0400

    Solution for 0219 contains-duplicate-ii

--- 0219-contains-duplicate-ii/solution.ts
+++ 0219-contains-duplicate-ii/solution.ts
@@ -1,18 +1,22 @@
 var containsNearbyDuplicate = function(nums, k) {
-    const numMap = new Map();
+  // Create a map to store the elements as keys and their indices as values
+  const numMap = new Map();
 
   for (let i = 0; i < nums.length; i++) {
-
+    // If the current element is already in the map, it means we have found a duplicate within the range 'k'
     if (numMap.has(nums[i])) {
       return true;
     }
 
+    // Add the current element to the map with a value of 'true'
     numMap.set(nums[i], true);
 
+    // If the size of the map exceeds the range 'k', remove the element that is 'k' steps behind the current index
     if (i >= k) {
       numMap.delete(nums[i - k]);
     }
   }
 
+  // If no duplicates are found within the range 'k', return false
   return false;
 };
