9a601bfa1af0ee9619ac3a260cc56a38ea66925e
Anderson Quinones <32976024+AndersonQuinones@users.noreply.github.com>
2023-07-16 12:20:43 -0400

    Solution for 0217 contains-duplicate

--- 0217-contains-duplicate/solution.ts
+++ 0217-contains-duplicate/solution.ts
@@ -1,3 +1,6 @@
 var containsDuplicate = function(nums) {
-      return new Set(nums).size !== nums.length;
+  // Create a new Set from the array, which automatically removes duplicates
+  // The size of the Set will be the number of unique elements in the array
+  // If the size of the Set is not equal to the length of the array, it means there are duplicates
+  return new Set(nums).size !== nums.length;
 };
