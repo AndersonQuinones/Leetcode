546b53857b078d984d30ab08ee1e5a57fafc14c3
Anderson Quinones <32976024+AndersonQuinones@users.noreply.github.com>
2023-07-19 17:56:39 -0400

    Solution for 2108 find-first-palindromic-string-in-the-array

---
+++ 2108-find-first-palindromic-string-in-the-array/solution.ts
@@ -0,0 +1,27 @@
+var isPalindrome = function(word) {
+    let left = 0;
+    let right = word.length - 1;
+
+    while(left < right) {
+        if (word[left] !== word[right]) {
+            return false
+        }
+
+        left++
+        right--
+    }
+
+
+    return true;
+}
+
+
+var firstPalindrome = function(words) {
+    
+    for (const word of words) {
+        if (isPalindrome(word)) {
+            return word;
+        }
+    }
+    return "";
+};
