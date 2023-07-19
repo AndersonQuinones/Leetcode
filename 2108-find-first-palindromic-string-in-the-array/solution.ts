29f7d3307abdf7d89d48952f7878b8f15e8c9f35
Anderson Quinones <32976024+AndersonQuinones@users.noreply.github.com>
2023-07-19 18:04:01 -0400

    Solution for 2108 find-first-palindromic-string-in-the-array

--- 2108-find-first-palindromic-string-in-the-array/solution.ts
+++ 2108-find-first-palindromic-string-in-the-array/solution.ts
@@ -1,27 +1,26 @@
-var isPalindrome = function(word) {
-    let left = 0;
-    let right = word.length - 1;
 
-    while(left < right) {
-        if (word[left] !== word[right]) {
-            return false
-        }
-
-        left++
-        right--
+var firstPalindrome =function(words){
+  for (const word of words) {
+    if (isPalindrome(word)) {
+      return word; // Return the word if it is a palindrome
     }
+  }
 
-
-    return true;
+  return ""; // Return an empty string if no palindrome is found
 }
 
+var isPalindrome = function(word) {
+  let left = 0; // Pointer for the leftmost character
+  let right = word.length - 1; // Pointer for the rightmost character
 
-var firstPalindrome = function(words) {
-    
-    for (const word of words) {
-        if (isPalindrome(word)) {
-            return word;
-        }
+  while (left < right) {
+    if (word[left] !== word[right]) {
+      return false; // If characters at left and right pointers don't match, it's not a palindrome
     }
-    return "";
-};
+
+    left++; // Move the left pointer towards the right
+    right--; // Move the right pointer towards the left
+  }
+
+  return true; // If the loop completes without returning false, the word is a palindrome
+}
