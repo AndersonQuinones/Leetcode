28492414f7f8d3cb26eea6d66892d8e2160544f2
Anderson Quinones <32976024+AndersonQuinones@users.noreply.github.com>
2024-01-15 23:18:01 -0500

    Solution for 0232 implement-queue-using-stacks

---
+++ 0232-implement-queue-using-stacks/solution.js
@@ -0,0 +1,49 @@
+var MyQueue = function() {
+    this.stack1 = [];
+    this.stack2 = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stack1.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    // If stack2 is empty, move elements from stack1 to stack2
+    if (this.stack2.length === 0) {
+        while (this.stack1.length > 0) {
+            this.stack2.push(this.stack1.pop());
+        }
+    }
+
+    // Pop the front element from stack2
+    return this.stack2.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    // If stack2 is empty, move elements from stack1 to stack2
+    if (this.stack2.length === 0) {
+        while (this.stack1.length > 0) {
+            this.stack2.push(this.stack1.pop());
+        }
+    }
+
+    // Peek the front element from stack2 without removing it
+    return this.stack2[this.stack2.length - 1];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.stack1.length === 0 && this.stack2.length === 0;
+};
